6d571baae28ef89cf34da5f31552b363f75a6b49
kx-commits <kyle.x.clark@gmail.com>
2021-04-06 22:25:01 -0700

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Kyle Xavier Clark on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
